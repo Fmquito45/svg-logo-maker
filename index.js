1ffc526b03e057643db05dea20442cc1de2429f2
Fmquito45 <fmquitox45@gmail.com>
2023-09-21 23:30:44 -0400

    adds question object and set init function

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
 const {Circle, Square, Triangle} = require('./lib/shapes')
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Enter up to 3 characters\n"
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Enter a color keyword (OR a hexadecimal number)\n"
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "Choose shape from list\n",
+        choices: ['Ciricle', 'Square', 'Triangle']
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a color keyword (OR a hexadecimal number)\n"
+    }
+]
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((responses) => {
+            console.log(responses)
+        })
+}
+
+init();
